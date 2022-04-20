f9c28855d6ac5efc177fba0095a34e68e7d2fe8c
Haritha Mohan <haritha@live.unc.edu>
2022-04-19 23:42:33 -0400

    home items initial

---
+++ src/data/homeItems.js
@@ -0,0 +1,70 @@
+import { Category } from "../models/items.ts";
+export const items1 = [
+ {
+   title: "Sweatshirt",
+   price: 10,
+   picture:
+     "https://i.etsystatic.com/31788734/r/il/901f0d/3567493314/il_1588xN.3567493314_9tfh.jpg",
+   category: Category.Clothing,
+   seller: "chiazo42",
+   date_posted: "04/14/2022",
+   tags: ["blue", "small"],
+   description: "description",
+ },
+ {
+   title: "455 Textbook",
+   price: 30,
+   picture:
+     "https://images-na.ssl-images-amazon.com/images/I/414EP0Tw76L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+   category: Category.School,
+   seller: "Haritha56",
+   date_posted: "04/10/2022",
+   tags: ["like new", "hardback"],
+   description: "description",
+ },
+ {
+   title: "Mini Fridge",
+   price: 50,
+   picture:
+     "https://u-mercari-images.mercdn.net/photos/m97592395723_1.jpg?width=1024&height=1024&format=pjpg&auto=webp&fit=crop&_=1624124090",
+   category: Category.Home,
+   seller: "Raven123",
+   date_posted: "04/11/2022",
+   tags: ["black", "Frigidaire"],
+   description: "description",
+ },
+];
+export const items2 = [
+ {
+   title: "Gel Manicure",
+   price: 15,
+   picture:
+     "https://s3-media0.fl.yelpcdn.com/bphoto/XFptVVoU8fP5Sul07x4XAQ/348s.jpg",
+   category: Category.Service,
+   seller: "Diane590",
+   date_posted: "04/13/2022",
+   tags: ["nails", "acrylic"],
+   description: "description",
+ },
+ {
+   title: "Keurig",
+   price: 20,
+   picture: "https://i.ebayimg.com/images/g/MScAAOSwVWth~rS7/s-l300.jpg",
+   category: Category.Home,
+   seller: "Samantha_Har",
+   date_posted: "04/09/2022",
+   tags: ["pink", "small"],
+   description: "description",
+ },
+ {
+   title: "Senior Photos",
+   price: 100,
+   picture:
+     "https://images.squarespace-cdn.com/content/v1/5fecb78061c78c299c559c6e/1613606588649-CGTLFHGOBA2UWA13V39O/20200403_Torrieri_GradPhotos_RachelGopichand-225.jpg",
+   category: Category.Service,
+   seller: "HannahB4",
+   date_posted: "04/08/2022",
+   tags: ["photography", "senior"],
+   description: "description",
+ },
+];
