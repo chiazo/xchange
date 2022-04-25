f0525c670b93eab23dc198109b9ef734888a9e91
Raven Taylor <raven61@live..unc.edu>
2022-04-24 20:06:33 -0400

    added user file

---
+++ src/data/user.js
@@ -0,0 +1,10 @@
+export const user = {
+  profile_pic: "",
+  name: "William Dunn",
+  username: "willyd23",
+  major: "Computer Science",
+  grad_term: "Spring 2022",
+  d_interests: ["Athletics", "Fashion", "CS Dept"],
+  m_interests: ["Sweaters", "Jewelry", "Textbooks"],
+  public: true,
+};
