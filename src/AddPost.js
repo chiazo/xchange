7538d5dc664d119de003c9d80f211ad934f095b3
Haritha Mohan <haritha@live.unc.edu>
2022-04-25 14:30:25 -0400

    event handlers for fields

--- src/AddPost.js
+++ src/AddPost.js
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import {user} from "./data/user";
+import { posts } from "./data/posts";
+import { Type } from "./models/post";
+import { Category } from "./models/homeItems";
 
 
 export const AddPost = () => {
+  
+  const [currPost, setCurrPost] = useState("");
+
+  const [currType, setCurrType] = useState("");
+  const [currCategory, setCurrCategory] = useState("");
+  const [currTitle, setCurrTitle] = useState("");
+  const [currText, setCurrText] = useState("");
+  const [currStatus, setCurrStatus] = useState("");
+  
+  let discussionSelect = false
+  
+  const updatePost = (e) => {
+    setCurrPost(e.target.value);
+    console.log("currPost: ", currPost);
+  }
+
+  const updateType = (e) => {
+    setCurrType(e.target.value);
+    discussionSelect = !discussionSelect;
+    console.log("currType: ", currType)
+  }
+
+  const updateCategory = (e) => {
+    setCurrType(e.target.value);
+    console.log("currCategory: ", currType)
+  }
+
+  const updateTitle = (e) => {
+    setCurrType(e.target.value);
+    console.log("currTitle: ", currType)
+  }
+
+  const updateText = (e) => {
+    setCurrType(e.target.value);
+    console.log("currText: ", currType)
+  }
+  
+  const updateStatus = (e) => {
+    setCurrType(e.target.value);
+    console.log("currStatus: ", currType)
+  }
+
   const submitPost =() => {
-    
+    const newPost = {
+      id: (posts.length),
+      type:Type.currType,
+      title:currTitle,
+      category:[Category.MainCategories.Academics],
+      status: currStatus,
+      author: user.username,
+      icon: user.icon,
+      date: new Date(),
+      upvotes:0,
+      downvotes:0,
+      upvoted:false,
+      downvoted:false,
+      comments:[]
+    }
+    posts.push(newPost)
   };
   return (
     <div className="home">
@@ -14,11 +75,11 @@ export const AddPost = () => {
       <legend class="col-form-label col-sm-2 pt-0">Type</legend>
       <div class="col-sm-10">
         <div class="custom-control custom-radio custom-control-inline">
-          <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input"></input>
+          <input onClick={updateType} value="Discussion" type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input"></input>
           <label class="custom-control-label" for="customRadioInline1">Discussion</label>
       </div>
       <div class="custom-control custom-radio custom-control-inline">
-        <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input"></input>
+        <input onClick={updateType} type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input"></input>
         <label class="custom-control-label" for="customRadioInline2">Q&amp;A</label>
       </div>
       </div>
@@ -28,7 +89,7 @@ export const AddPost = () => {
   <legend class="col-form-label col-sm-2 pt-0">Category</legend>
     <div class="col-sm-10">
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"></input>
+        <input onClick={updateCategory} value="Academics" class="form-check-input" type="checkbox" id="inlineCheckbox1" ></input>
         <label class="form-check-label" for="inlineCheckbox1">Academics</label>
       </div>
       <div class="form-check form-check-inline">
@@ -40,13 +101,13 @@ export const AddPost = () => {
   <div class="form-group row">
     <label for="inputEmail3" class="col-sm-2 col-form-label">Title</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3"></input>
+      <input type="email" class="form-control" id="inputEmail3" onChange={updateTitle}></input>
     </div>
   </div>
   <div class="form-group row">
     <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Text</label>
     <div class="col-sm-10">
-      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <textarea onChange={updateText} class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
     </div>
   </div>
 
