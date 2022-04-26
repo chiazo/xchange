e7b75f24f6f762d9ddec31285e9ff1bd07f9d35b
chiazo <chiazo@live.unc.edu>
2022-04-26 11:59:14 -0400

    remove duplicate edit button and add profile pic

--- src/Profile.js
+++ src/Profile.js
@@ -1,17 +1,12 @@
 import React from "react";
+import { user } from "./data/user";
 
 const Profile = () => {
   return (
     <div className="home">
       <div id="content">
         <div className="profile-edit">
-          <img
-            id="profile-pic"
-            alt="profile"
-            src={
-              "https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
-            }
-          />
+          <img id="profile-pic" alt="profile" src={user.icon} />
           <div className="edit-box">
             <img
               alt="edit"
@@ -21,7 +16,7 @@ const Profile = () => {
           </div>
         </div>
         <h1 id="profile-name">William Dunn</h1>
-        <h id="profile-username">@willyd23</h>
+        <div id="profile-username">@willyd23</div>
         <div id="profile" className="about-me">
           <h1>About Me</h1>
           <div className="row">
@@ -29,18 +24,11 @@ const Profile = () => {
               <p>Major: Computer Science</p>
               <p>Graduation Term: Spring 2022</p>
             </div>
-            <div className="edit-box2">
-              <img
-                alt="edit"
-                id="edit-icon"
-                src="https://img.icons8.com/ios/50/000000/edit--v1.png"
-              />
-            </div>
           </div>
         </div>
         <div id="profile">
           <h1>Discussion Interests</h1>
-          <div class="interest">
+          <div className="interest">
             <p>x Athletics</p>
             <p>x Fashion</p>
             <p>x CS Dept</p>
@@ -55,11 +43,10 @@ const Profile = () => {
         </div>
         <div id="profile" className="market-interests">
           <h1>Market Interests</h1>
-          <div class="interest">
+          <div className="interest">
             <p>x Sweaters</p>
             <p>x Jewelry</p>
             <p>x Textbooks</p>
-
             <div className="edit-box3">
               <img
                 alt="edit"
@@ -71,12 +58,12 @@ const Profile = () => {
         </div>
         <div id="profile" className="privacy-settings">
           <h1>Privacy Settings</h1>
-          <div class="interest">
+          <div className="interest">
             <p id="field">Status</p>
             <p id="status">By William</p>
             <p id="status">By Anon</p>
           </div>
-          <div class="interest">
+          <div className="interest">
             <p id="field">Password</p>
             <p id="password">******</p>
           </div>
--- src/data/user.js
+++ src/data/user.js
@@ -3,7 +3,9 @@ import { Status } from "../models/post.ts";
 export const user = {
   profile_pic: "",
   name: "William Dunn",
-  icon: "https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
+  icon:
+    "https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ" ||
+    "https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png",
   username: "willyd23",
   major: "Computer Science",
   grad_term: "Spring 2022",
