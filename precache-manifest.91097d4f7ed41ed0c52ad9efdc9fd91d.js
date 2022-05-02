4d3734f81e8a680def2d2034e1c37d33da0be4a5
Chiazo Agina <chiazo@live.unc.edu>
2022-05-02 15:02:46 -0400

    Updates

---
+++ precache-manifest.91097d4f7ed41ed0c52ad9efdc9fd91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aedf487da2e8d8d37267",
+    "url": "/xchange/static/css/main.288fa157.chunk.css"
+  },
+  {
+    "revision": "aedf487da2e8d8d37267",
+    "url": "/xchange/static/js/main.aedf487d.chunk.js"
+  },
+  {
+    "revision": "8fd13db900db69b037d6",
+    "url": "/xchange/static/css/1.765c3b56.chunk.css"
+  },
+  {
+    "revision": "8fd13db900db69b037d6",
+    "url": "/xchange/static/js/1.8fd13db9.chunk.js"
+  },
+  {
+    "revision": "6533b05301c925e76b80",
+    "url": "/xchange/static/js/runtime~main.6533b053.js"
+  },
+  {
+    "revision": "5645acd8ebed3ac505f8962b3ae2c059",
+    "url": "/xchange/index.html"
+  }
+];
